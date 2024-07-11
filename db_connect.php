8bd34ac5e94504068ab21334addc517476ea0a88
daraayo <156916992+daraayo@users.noreply.github.com>
2024-07-11 14:37:39 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$serverName = "localhost\\SQLEXPRESS"; // adjust if your instance name is different
+$serverName = "DESKTOP-O3R36AQ"; // adjust if your instance name is different
 $connectionInfo = array(
     "Database" => "skills_assessment_quiz",
     "UID" => "quizapp",
